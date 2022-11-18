66b69daf5d4a82de3b7bf571d6adfe2166453ff2
Yash Potdukhe <79392684+YashPotdukhe@users.noreply.github.com>
2022-11-18 09:32:08 -0500

    Add files via upload

---
+++ js/personal.js
@@ -0,0 +1,41 @@
+// document.querySelector("card").addEventListener("click", function() {
+//     console.log("image 1 clicked");
+    
+// });
+
+const card = document.getElementById("card");
+card.addEventListener("click", function() {
+    card.classList.toggle("flipCard");
+});
+
+document.querySelector("#home_item2").addEventListener("click", function() {
+    console.log("image 2 clicked");
+});
+
+document.querySelector("#home_item3").addEventListener("click", function() {
+    console.log("image 3 clicked");
+});
+
+document.querySelector("#home_item4").addEventListener("click", function() {
+    console.log("image 4 clicked");
+});
+
+document.querySelector("#home_item5").addEventListener("click", function() {
+    console.log("image 5 clicked");
+});
+
+document.querySelector("#home_item6").addEventListener("click", function() {
+    console.log("image 6 clicked");
+});
+
+document.querySelector("#home_item7").addEventListener("click", function() {
+    console.log("image 7 clicked");
+});
+
+document.querySelector("#home_item8").addEventListener("click", function() {
+    console.log("image 8 clicked");
+});
+
+document.querySelector("#home_item9").addEventListener("click", function() {
+    console.log("image 9 clicked");
+});
